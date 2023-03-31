bd75973724d39afdbd3c43b9bc63fa1d37547303
aluno <Aluno@PR02-402-58.CSED.LAB>
2023-03-31 11:24:48 -0300

    atualização aula 31-03

--- api_js/src/index.js
+++ api_js/src/index.js
@@ -1,7 +1,11 @@
+const { request, response } = require('express');
 const express = require('express');
 
 // Criando um servidor através do express
 const app = express();
+
+// Configurar a aplicação para receber dados no formato JSON
+app.use(express.json());
 
 // Configurando uma requisição com o método GET para a raiz da aplicação
 app.get("/", function(request, response){
@@ -9,6 +13,28 @@ app.get("/", function(request, response){
     response.status(200).json({ message : "Resposta enviada em JSON"});
 });
 
+// EXERCÍCIO - Criar uma nova rota/funcionalidade para receber uma informação pela URL da requisição
+
+app.get("/:categoria/:produto", (request, response) => { // pode ser feito assim também, o => substitui o function
+    // const produto = request.params.produto;
+    // const categoria = request.params.categoria;
+    // ou
+    const { produto, categoria} = request.params;
+    console.log(produto, categoria);
+
+    response.status(200).json({message : "Categoria: " + categoria + " Produto: " + produto});
+
+    // console.log(request.params.produto, request.params.categoria);
+})
+
+
+
+app.post("/", (request, response) => {
+    // console.log(request.body); // para mostrar no terminal
+    response.status(201).json({ message : "Categoria cadastrada!", categoria : request.body });
+});
+
+// 
 
 
 // O comando listen roda a aplicação
